0e04e678331ee0fa534ae4ad70d74820338b11d2
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-09-08 08:43:06 +0530

    Delete server directory

--- server/routes/review.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const controllers = require("../controllers/reviewerController");
-const router = express.Router();
-
-router.post("/review/:id", controllers.reviewQuestion);
-
-module.exports = router;
--- server/validations/subjectValidation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const createSubjectSchema = Joi.object({
-  name: Joi.string().required().min(3).max(100)
-});
-
-module.exports = {
-  createSubjectSchema
-};
